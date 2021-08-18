b3b2bd11776ec0a837677829f3f69cdcebae925d
algmoreno <82549378+algmoreno@users.noreply.github.com>
2021-08-17 19:16:17 -0700

    Delete mod17 directory

--- mod17/practice.js
+++
@@ -1,9 +0,0 @@
-const containerEl = document.getElementById('container');
-
-const clickHandler = function(event) {
-  if (event.target.matches('button')) {
-    event.target.textContent = 'Clicked!';
-  }
-};
-
-containerEl.addEventListener('click', clickHandler);
